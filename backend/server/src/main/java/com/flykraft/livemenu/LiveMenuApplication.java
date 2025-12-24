c0d7ecb3a64091f0a23befb89ee04e94aaa03c7d
amankr8 <amankumar141099@gmail.com>
2025-12-24 12:06:02 +0530

    code refactor

--- backend/server/src/main/java/com/flykraft/livemenu/LiveMenuApplication.java
+++ backend/server/src/main/java/com/flykraft/livemenu/LiveMenuApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MenuServiceApplication {
+public class LiveMenuApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MenuServiceApplication.class, args);
+		SpringApplication.run(LiveMenuApplication.class, args);
 	}
 
 }
