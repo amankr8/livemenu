abe6f1fb74a1e89cedf88c3761942d0ebbae86d4
amankr8 <amankumar141099@gmail.com>
2026-03-25 19:30:38 +0530

    code update

--- frontend/src/app/pages/components/menu/menu.component.ts
+++ frontend/src/app/pages/components/menu/menu.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { MenuItemCardComponent } from '../menu-item-card/menu-item-card.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Icons } from '../../../utils/icons';
+import { CategoryService } from '../../../service/category.service';
 
 @Component({
   selector: 'app-menu',
@@ -15,28 +16,50 @@ export class MenuComponent {
   icons = Icons;
 
   private menuService = inject(MenuService);
+  private categoryService = inject(CategoryService);
+
   menuItems = this.menuService.menuItems;
   loading = this.menuService.loading;
   error = this.menuService.error;
 
+  categories = this.categoryService.categories;
+
   ngOnInit(): void {
     this.menuService.loadMenuItems();
+    this.categoryService.loadCategories();
   }
 
   trackById = (_: number, item: MenuItem) => item.id;
 
   groupedMenuItems = computed(() => {
     const items = this.menuItems() || [];
-    const groups = items.reduce((acc: any, item) => {
-      const cat = item.categoryId || 'Other';
-      if (!acc[cat]) acc[cat] = [];
-      acc[cat].push(item);
+    const categories = this.categories() || [];
+
+    const groups = items.reduce((acc: { [key: string]: MenuItem[] }, item) => {
+      const catKey = item.categoryId
+        ? item.categoryId.toString()
+        : 'uncategorized';
+
+      if (!acc[catKey]) acc[catKey] = [];
+      acc[catKey].push(item);
       return acc;
     }, {});
 
-    return Object.keys(groups).map((category) => ({
-      label: category,
-      items: groups[category],
-    }));
+    return Object.keys(groups).map((key) => {
+      if (key === 'uncategorized') {
+        return {
+          label: 'Other Items',
+          items: groups[key],
+        };
+      }
+
+      const categoryId = Number(key);
+      const categoryObj = categories.find((c) => c.id === categoryId);
+
+      return {
+        label: categoryObj ? categoryObj.name : 'Unknown',
+        items: groups[key],
+      };
+    });
   });
 }
