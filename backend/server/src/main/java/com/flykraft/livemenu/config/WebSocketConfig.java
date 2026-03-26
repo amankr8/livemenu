7442269547029c609d010d0e54d414c4c9c93ffd
amankr8 <amankumar141099@gmail.com>
2026-03-26 21:11:47 +0530

    code update

--- backend/server/src/main/java/com/flykraft/livemenu/config/WebSocketConfig.java
+++ backend/server/src/main/java/com/flykraft/livemenu/config/WebSocketConfig.java
@@ -19,6 +19,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-orders")
-                .setAllowedOrigins("*");
+                .setAllowedOriginPatterns(
+                        "https://*.livemenu.in",
+                        "https://*.lvh.me"
+                );
     }
 }
